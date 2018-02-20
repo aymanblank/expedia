3c0a26aae4ecdc85eee3076bc67991fe7bb274ac
Ayman Abu Khalifa <Aymanblank@gmail.com>
2018-02-20 20:33:42 +0200

    handling post requests to /api/hotels by calling expedia api and returning the received response

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
+// expedia api url 
+const API = 'https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel';
+
+// initializing express app
 const app = express();
 
-app.get('/api/', (req, res) => {
-  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// creating a post route to retrieve filtered hotels 
+app.post('/api/hotels', (req, res) => {
+    // assigning request params to a variable or leave it empty if there is none
+    const params = req.body || {};
+
+    // calling expedia api with all params 
+    axios.get(API,{params})
+    .then(function (response) {
+        // sending back the response data 
+        res.send(response.data);
+    })
+    .catch(function (error) {
+        // catching any error that could occur and sending it back with a status of 500
+        res.status(500).send('Calling Expedia Api Faild because: ' + error);
+    });
 });
 
-const port = 5000;
+// setting a port for the server to listen to
+const port = 5050;
 
+// initializing server listener on the port defined 
 app.listen(port, () => `Server running on port ${port}`);
