364cabafab44469186f3d4a41eabf6cd6b0275b0
Ayman Abu Khalifa <Aymanblank@gmail.com>
2018-02-20 20:20:09 +0200

    Prepared the app.js by importing all required packages and components, also added the provider for the store and subscribed to it. setting Loading, Hotels & AppBar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import FlatButton from 'material-ui/FlatButton';
+import AppBar from 'material-ui/AppBar';
+import { Provider } from 'react-redux'
+import store from './store'
+import Hotels from './components/Hotels';
+import Loading from './components/Loading';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 class App extends Component {
+  constructor(){
+    super();
+
+    //initial state
+    this.state = {
+      load : false
+    };
+
+    // function to handle any changes to the store
+    // currently only checking for loading state and assign it to load in this.state
+    const handleChange = () => {
+      const state = store.getState();
+      this.setState({load: state.loading});
+    }
+
+    // subscribing and listening to store events
+    store.subscribe(handleChange);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={ store }>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <Loading load={this.state.load} />
+          <MuiThemeProvider muiTheme={muiTheme}>
+            <AppBar
+              title="Expedia Coding Exercise"
+              iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+              iconElementRight={<FlatButton label="Ayman Abu Khalifa" 
+                onClick={ event => window.open('https://www.linkedin.com/in/ayman-abu-khalifa-22bb4a94/') } />}
+            />
+          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={muiTheme}>
+            <Hotels />
+          </MuiThemeProvider>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
